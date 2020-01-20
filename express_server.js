55f2f8275d44167563fe6574f30dfbc61e8db493
Lian Yelich <lianyelich14@gmail.com>
2020-01-20 21:48:51 +0000

    Fixed a typo inside of express_server.js

--- express_server.js
+++ express_server.js
@@ -14,8 +14,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls, urlDatabase };
+  let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
