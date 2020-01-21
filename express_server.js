c6e4eb033a89150682587397359de9367299fdd5
Lian Yelich <lianyelich14@gmail.com>
2020-01-21 01:14:35 +0000

    Added the urls_new.ejs and the generateRandomString.js function to the functions directory

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
+const generateRandomString = require('./functions/generateRandomString');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouse.ca",
@@ -8,22 +10,31 @@ const urlDatabase = {
 };
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
+//========GET================================================================
+//Standard
 app.get("/", (req, res) => {
   res.send('Hello');
 });
 
-//for urls_index in views
+//for urls_index in ./views
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-//for urls_show in views
+//for urls_new in ./views
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//for urls_show in ./views
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -32,7 +43,14 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>");
 });
+//============POST==============================================
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Alright, bud");
+});
+
+//============LISTEN============================================
 app.listen(PORT, () => {
   console.log(`Example listening on port: ${PORT}`);
 });
---
+++ functions/generateRandomString.js
@@ -0,0 +1,17 @@
+const generateRandomString = function() {
+  const randomNum = function() {
+    return (Math.floor(Math.random(122) * 100));
+  };
+  let results = "";
+  while (results.length < 6) {
+    randomInt = randomNum();
+    if ((randomInt >= 97 && randomInt <= 122) || randomInt >= 65 && randomInt <= 90) {
+      results += (String.fromCharCode(randomInt));
+    } else if (randomInt < 10) {
+      results += randomInt;
+    }
+  }
+  return results;
+}
+
+modules.export = { generateRandomString };
